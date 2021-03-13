a2a379d68470ec8586220c7242d4b6dc6d0441c2
aminelch <aminelch95@gmail.com>
2021-03-13 20:41:30 +0100

    updating package name, readme.md file , adding License in comments

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -1,31 +1,38 @@
-<?php 
+<?php
+
+/*
+ * This file is part of the PHPsee package.
+ *
+ * (c) Amine L'ch <aminelch95@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 
-namespace PhpSee\Helpers ; 
+namespace PhpSee\Helpers;
 
- class Str { 
+class Str
+{
 
+/**
+ * Generate a random string 
+ *
+ * @param integer $length
+ * @param boolean $symbol make use of symbols 
+ * @param boolean $upper make use Upper case chars 
+ * @return string 
+ */
+    public static function randomString(int $length = 20, bool $symbol = true, bool $upper = true) :string
+    {
 
-    public static function sayHello() : string{
+        $a_pattern = 'abcdefghijklmnopqrstuvwxyz';
+        $a_maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $a_symb = "#{[|``\^@]}-_*/!,;";
 
-        return sprintf("hello word"); 
+        if ($symbol) $a_pattern .= $a_symb;
+        if ($upper) $a_pattern .= $a_maj;
+
+        return str_shuffle(substr($a_pattern, 0, $length  - 1));
     }
-
-    
-
-    public static function randomString(int $length = 20 , bool $symbol=true,bool $maj=true){
-
-        $a_pattern='abcdefghijklmnopqrstuvwxyz';
-        $a_maj="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        $a_symb="#{[|``\^@]}-_*/!,;";
-
-        if($symbol)$a_pattern.=$a_symb;
-        if($maj) $a_pattern.=$a_maj;
-        
-        return str_shuffle(substr($a_pattern,0,$length  -1 )); 
-
-    }
-
-
-
 }
